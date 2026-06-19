bc74ff7198aeee2f2266f1e5f2369aa18b24d058
krishitha1310 <krishitha174@gmail.com>
2026-06-19 14:19:45 +0530

    java script basucs

---
+++ frontEnd/javascript-practice.js
@@ -0,0 +1,51 @@
+// console.log("Welcome to javaScript")
+// var a=10;
+// var b="10"
+// console.log(a===b)
+
+// function greet(){
+//     var name="abc"
+//     console.log(name)
+// }
+// console.log(name)
+
+// if(true){
+//     let c=100
+//     console.log(c)
+// }
+// console.log(c)
+
+
+// let a=45
+// console.log(a)
+// let a="krishi"
+// console.log(a)
+
+
+// let a=47
+// console.log(a)
+// a=56
+// console.log(a)
+
+
+// const a=45
+// console.log(a)
+// const a="krishi"
+// console.log(a)
+
+// const a=47
+// console.log(a)
+// a=56
+// console.log(a)
+
+
+// var a=10
+// var b="25"
+// console.log(a+b)
+
+const a=1
+const b=2
+const c=3
+if(a>b&&a>c) console.log("a is greater:"+a)
+else if(b>c&&b>a) console.log("B is greater:"+b)
+else console.log("C is greater:"+c)
