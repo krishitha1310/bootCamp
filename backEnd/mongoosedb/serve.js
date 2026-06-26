3c69cff8b49878a0a780c61f94899bd10a286c57
krishitha1310 <krishitha174@gmail.com>
2026-06-26 23:04:10 +0530

    connection established between frontend and backend

--- backEnd/mongoosedb/serve.js
+++ backEnd/mongoosedb/serve.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const { error } = require("node:console");
 
-const app = express();
+const app = express();  
 
 app.use(express.json());
 const cors=require("cors");
